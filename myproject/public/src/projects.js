f0b86908d668ff813eff249017e38da6c92cab7e
Johnny <johnny@scottmail.ca>
2023-04-28 19:31:15 -0700

    Removed squaredy cat video from my website, until i get permission

--- myproject/public/src/projects.js
+++ myproject/public/src/projects.js
@@ -1,9 +1,10 @@
+//https://www.youtube.com/embed/urQ51v-mU5M?start=850
 let projects = [
     {
         name: 'Squaredy Cat',
         description: 'Squaredy Cat is a short horror game made over the course of 3 days for the Mini Jam 127 game jam hosted on Itch.io. The theme for the Game Jam was “Cats2” and the limitation was to only use a predetermined color palette. Squaredy Cat ended up coming in 11th out of 121 entries.',
         tags: 'Unity/C#, Blender, Gimp, game',
-        video: 'https://www.youtube.com/embed/urQ51v-mU5M?start=850',
+        video: 'https://www.youtube.com/embed/PJ9JRmcD2h0',
         github: 'https://github.com/JG-Scott',
         imageGallery: '/static/squaredyCat.png',
         imageSingular: '/static/squaredyCat.png'
