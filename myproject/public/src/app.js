140bc26c5e500d96607c2fbbfcf84f56447cccad
Johnny <johnny@scottmail.ca>
2023-05-07 15:58:13 -0700

    added art section

--- myproject/public/src/app.js
+++ myproject/public/src/app.js
@@ -1,269 +1,37 @@
-let tools = new Object();
+var projectCards;
+var projectName;
+var projectImage;
+var projectDetails;
+var projectVideo;
+var itchBtn;
+var gitBtn;
+var skillSection;
+var artImage;
 
-tools = {
-    "CSharp": {
-        image: 'Csharp_Logo.png',
-    },
-    "CPlusPlus": {
-        image: 'CLogo.png',
-    },
-    "Blender": {
-        image: 'Blender_logo.png',
-    },
-    "Unity": {
-        image: 'unity-69-logot.png',
-    },
-    "GIMP": {
-        image: 'The_GIMP.png',
-    },
-    "Javascript": {
-        image: 'JavaScript-logo.png',
-    },
-    "Typescript": {
-        image: 'Typescript_logo.png',
-    },
-    "Java": {
-        image: 'java-logo.png',
-    },
-    "React": {
-        image: 'React-icon.png',
-    },
-    "Git": {
-        image: 'git.png',
-    },
-    "Aseprite": {
-        image: 'aseprite.png',
-    },
-    "Audacity": {
-        image: 'Audacity_Logo.png',
-    },
-}
 
 
 
 let width = screen.width;
-const projectInfo = document.querySelector('#projectInfo');
+const projectInfo = document.querySelector('#projectInfoHolder');
 //change project section when below 992
-if(width >= 992) {
-    console.log(width)
-    projectInfo.innerHTML = `        <div class="projectDetails">
-          <div class="imageVideo">
-            <img src="static/squaredyCat.png" class="image" alt="" />
-                       <div style="width: 100%; max-width: 550px">
-              <div
-                style="
-                  position: relative;
-                  padding-bottom: 56.15%;
-                  height: 0;
-                  overflow: hidden;
-                "
-              >
-                <iframe
-                  style="
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    width: 100%;
-                    height: 100%;
-                    border: 0;
-                  "
-                  loading="lazy"
-                  ;
-                  class="video"
-                  src=""
-                  srcdoc="<style>
-          * {
-          padding: 0;
-          margin: 0;
-          overflow: hidden;
-          }
-          
-          body, html {
-            height: 100%;
-          }
-          
-          img, svg {
-            position: absolute;
-            width: 100%;
-            top: 0;
-            bottom: 0;
-            margin: auto;
-          }
-          
-          svg {
-            filter: drop-shadow(1px 1px 10px hsl(206.5, 70.7%, 8%));
-            transition: all 250ms ease-in-out;
-          }
-          
-          body:hover svg {
-            filter: drop-shadow(1px 1px 10px hsl(206.5, 0%, 10%));
-            transform: scale(1.2);
-          }
-        </style>
-        <a  href=''>
-          <img src='https://img.youtube.com/vi/PJ9JRmcD2h0/hqdefault.jpg' alt='Coffee Recipe Javascript Project'>
-          <svg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 24 24' fill='none' stroke='#ffffff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-play-circle'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg>
-        </a>
-        "
-                  title="Coffee Recipe Javascript Project"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen
-                >
-                </iframe>
-              </div>
-            </div>
-          </div>
-          <h2 class="name">Project name</h2>
-          <div class="detailArea">
-          <p class="details">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-            atque nulla, distinctio neque qui repellat sequi quasi asperiores
-            iste. Atque rerum magnam suscipit nam nulla perspiciatis ratione
-            vitae. Dicta, cupiditate.
-          </p>
-          <div class="skillArea">
-
-          <div class="skillContainer">
-
-        </div>
-        </div>
-          </div>
-          <div class="btnGroup">
-            <a href="#" target="_blank" class="btn light" id="gitBtn">GitHub</a>
-            <a href="#" target="_blank" class="btn transparent" id="itchBtn"
-              >Itch.io</a
-            >
-          </div>
-        </div>`
-} else {
-    projectInfo.innerHTML=`        <div class="projectDetails">
-          <div class="imageDescription">
-            <img src="static/squaredyCat.png" class="image" alt="" />
-            <h2 class="name">Project name</h2>
-            <p class="details">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-              atque nulla, distinctio neque qui repellat sequi quasi asperiores
-              iste. Atque rerum magnam suscipit nam nulla perspiciatis ratione
-              vitae. Dicta, cupiditate.
-            </p>
-          </div>
-          <div class="btnGroup">
-            <a href="#" target="_blank" class="btn light" id="gitBtn">GitHub</a>
-            <a href="#" target="_blank" class="btn transparent" id="itchBtn"
-              >Itch.io</a
-            >
-          </div>
-          <div class="skillContainer">
-
-        </div>
-          <div class="videoSection">
-                        <div style="width: 100%; max-width: 550px">
-              <div
-                style="
-                  position: relative;
-                  padding-bottom: 56.15%;
-                  height: 0;
-                  overflow: hidden;
-                "
-              >
-                <iframe
-                  style="
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    width: 100%;
-                    height: 100%;
-                    border: 0;
-                  "
-                  loading="lazy"
-                  ;
-                  class="video"
-                  src=""
-                  srcdoc="<style>
-          * {
-          padding: 0;
-          margin: 0;
-          overflow: hidden;
-          }
-          
-          body, html {
-            height: 100%;
-          }
-          
-          img, svg {
-            position: absolute;
-            width: 100%;
-            top: 0;
-            bottom: 0;
-            margin: auto;
-          }
-          
-          svg {
-            filter: drop-shadow(1px 1px 10px hsl(206.5, 70.7%, 8%));
-            transition: all 250ms ease-in-out;
-          }
-          
-          body:hover svg {
-            filter: drop-shadow(1px 1px 10px hsl(206.5, 0%, 10%));
-            transform: scale(1.2);
-          }
-        </style>
-        <a  href=''>
-          <img src='https://img.youtube.com/vi/PJ9JRmcD2h0/hqdefault.jpg' alt='Coffee Recipe Javascript Project'>
-          <svg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 24 24' fill='none' stroke='#ffffff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-play-circle'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg>
-        </a>
-        "
-                  title="Coffee Recipe Javascript Project"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen
-                >
-                </iframe>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>`;
+// if(width >= 992) {
+//     console.log(width)
+//     projectInfo.innerHTML = largeProjectSection;
+// } else {
+//     projectInfo.innerHTML= smallProjectSection;
+// }
+const grabHTML = () => {
+    projectCards = [...document.querySelectorAll('.projectCard')];
+    projectName = document.querySelector('.projectDetails .name');
+    projectImage = document.querySelector('.projectDetails .image');
+    projectDetails = document.querySelector('.projectDetails .details');
+    projectVideo = document.querySelector(' .video');
+    itchBtn = document.querySelector('#itchBtn');
+    gitBtn = document.querySelector('#gitBtn');
+    skillSection = document.querySelector('.skillContainer');
+    artImage = document.querySelector('.artImage');
 }
-
-
-
-
-
-
-
-
-let projectCards = [...document.querySelectorAll('.projectCard')];
-
-
-let projectName = document.querySelector('.projectDetails .name');
-let projectImage = document.querySelector('.projectDetails .image');
-let projectDetails = document.querySelector('.projectDetails .details');
-let projectVideo = document.querySelector(' .video');
-
-
-
-let itchBtn = document.querySelector('#itchBtn');
-let gitBtn = document.querySelector('#gitBtn');
-
-let skillSection = document.querySelector('.skillContainer');
-
-
-console.log(projectName);
-
-
-
-
-
-
-
-
-
-
-
-
-
+grabHTML();
 
 projectCards.map((project, i) => {
     project.addEventListener('click', () => {
@@ -278,8 +46,25 @@ projectCards.map((project, i) => {
 
 
 const setUpInfo = (data) => {
+  console.log("this is running")
+  if(data.filter == 'game') {
+    handleProjectSection(data);
+  } else if(data.filter == 'art') {
+    handleArtSection(data);
+  }
+      
+}
 
-        projectName.innerHTML = data.name;
+
+const handleProjectSection = (data) => {
+  if(width >= 992) {
+      console.log(width)
+      projectInfo.innerHTML = largeProjectSection;
+  } else {
+      projectInfo.innerHTML= smallProjectSection;
+  }
+  grabHTML();
+  projectName.innerHTML = data.name;
         if(screen.width <= 732) {
             projectImage.src = data.imageGallery;
 
@@ -343,9 +128,16 @@ const setUpInfo = (data) => {
                                       </div>
                                      </div>`
         });
+} 
+
+const handleArtSection = (data) => {
+  
+  projectInfo.innerHTML = artSection;
+  grabHTML();
+  artImage.src = data.imageGallery;
+  projectName.innerHTML = data.name;
+  projectDetails.innerHTML = data.description;
 }
-
-
 
 
 
@@ -369,6 +161,14 @@ filters.map((btn, i) => {
 
 });
 setUpInfo(projects[0]);
+
+projectCards.map(project => {
+    if(!project.getAttribute('data-tags').includes('game')) {
+        project.style.display = 'none';
+    } else {
+        project.style.display = null;
+    }
+})
 
 const navbar = document.querySelector('nav');
 
