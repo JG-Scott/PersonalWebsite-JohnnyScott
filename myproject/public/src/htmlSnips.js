9afbfa07e719c4591ed22b5d075b9e47d5e218d2
Johnny <johnny@scottmail.ca>
2023-05-07 21:25:12 -0700

    added click art view

--- myproject/public/src/htmlSnips.js
+++ myproject/public/src/htmlSnips.js
@@ -186,6 +186,19 @@ const smallProjectSection = `<div id="projectInfo"><div class="projectDetails">
 let artSection = `<div class='ArtInfo'><div class="projectDetails">
           <div class="artImageSection">
             <img src="static/christmas.png" class="artImage" alt="" />
+            
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- The Close Button -->
+  <span class="close">&times;</span>
+
+  <!-- Modal Content (The Image) -->
+  <img class="modal-content" id="img01">
+
+  <!-- Modal Caption (Image Text) -->
+  <div id="caption"></div>
+</div>
           </div>
           <h2 class="name">Project name</h2>
           <div class="detailArea">
